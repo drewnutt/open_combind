8da4d5e8aca8cfa5deba04e3fee3a5438f24e804
Drew McNutt <anm329@pitt.edu>
2022-04-15 15:01:46 -0400

    Ligands now sdf files
    Also cleans up the lig.pdb after the sdf file is made

--- dock/grid.py
+++ dock/grid.py
@@ -1,42 +1,19 @@
 import os
 import subprocess
 from glob import glob
-# from schrodinger.structure import StructureReader
-# from schrodinger.structutils.transform import get_centroid
 
-# GRID_IN = """
-# GRID_CENTER {x},{y},{z}
-# GRIDFILE {pdb}.zip
-# INNERBOX 15,15,15
-# OUTERBOX 30,30,30
-# RECEP_FILE {prot}
-# """
-
-# CMD = "glide -WAIT {infile}"
 CMD = "gnina -r {prot} --autobox_ligand {abox_lig} "
-
-# not necessary because Gnina can use ligand to define autobox
-# def centroid(ligfile):
-#     with StructureReader(ligfile) as st:
-#         st = next(st)
-#     c = get_centroid(st)
-#     x,y,z = c[:3]
-#     return x, y, z
 
 def make_grid(pdb,
               PROTFILE='structures/proteins/{pdb}_prot.pdb',
-              LIGFILE='structures/ligands/{pdb}_lig.mol2',
+              LIGFILE='structures/ligands/{pdb}_lig.sdf',
               DOCKTEMP='structures/template/{pdb}.template'):
-    # if grid_in is None:
-    #     grid_in = GRID_IN
 
     ligfile = os.path.abspath(LIGFILE.format(pdb=pdb))
     protfile = os.path.abspath(PROTFILE.format(pdb=pdb))
     docktemp = os.path.abspath(DOCKTEMP.format(pdb=pdb))
     cmd = CMD.format(prot=protfile,abox_lig=ligfile)
 
-    # if os.path.exists(zipfile):
-    #     return # Done.
     if not (os.path.exists(ligfile) and os.path.exists(protfile)):
         print(ligfile, protfile)
         return # Not ready.
@@ -48,10 +25,3 @@ def make_grid(pdb,
 
     with open(docktemp, 'w') as template:
         template.write(cmd)
-
-    # x, y, z = centroid(ligfile)
-
-    # with open(infile, 'w') as fp:
-    #     fp.write(grid_in.format(x=x, y=y, z=z, pdb=pdb, prot=protfile))
-
-    # subprocess.run(cmd, cwd=cwd, shell=True)
--- dock/struct_sort.py
+++ dock/struct_sort.py
@@ -1,24 +1,22 @@
 import os
 from prody import parsePDB, writePDB
 from plumbum.cmd import obabel
-# from schrodinger.structure import StructureReader
 
 def split_complex(st, pdb_id):
     os.system('mkdir -p structures/proteins structures/ligands')
-    lig_path = 'structures/ligands/{}_lig.mol2'.format(pdb_id)
+    lig_path = 'structures/ligands/{}_lig.sdf'.format(pdb_id)
     prot_path = 'structures/proteins/{}_prot.pdb'.format(pdb_id)
     lig_pdb_path = 'structures/ligands/{}_lig.pdb'.format(pdb_id)
 
     if not os.path.exists(lig_path):
         lig_st = st.select('hetero')
-        # lig_st.title = '{}_lig'.format(pdb_id)
         writePDB(lig_pdb_path,lig_st)
         obabel[lig_pdb_path, '-O', lig_path]()
+        os.remove(lig_pdb_path)
         
     
     if not os.path.exists(prot_path):
         prot_st = st.select('protein')
-        # prot_st.title = '{}_prot'.format(pdb_id)
         writePDB(prot_path,prot_st)
 
 def struct_sort(structs):
