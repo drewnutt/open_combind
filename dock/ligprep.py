e2ad6a867d9fc33f8d9c3408321f5c59264d1f69
Drew McNutt <anm329@pitt.edu>
2022-04-15 14:55:53 -0400

    Ligprep with large SDF file
    Instead of creating a csv with smiles and names, now can use a large SDF
    file, that is either 2D or 3D, that contains all of the ligands to create
    all of the individual ligand sdfs.

--- dock/ligprep.py
+++ dock/ligprep.py
@@ -1,5 +1,6 @@
 # from schrodinger.structure import StructureReader, StructureWriter
 import os
+import gzip
 import subprocess
 from plumbum.cmd import obabel
 from rdkit.Chem import AllChem as Chem
@@ -17,15 +18,34 @@ def ligprocess(input_file, output_file):
 
 def ligprep(smiles):
     sdf_file = smiles.replace('.smi', '.sdf')
-    # cmd = 'ligprep -WAIT -epik -ismi {} -omae {}'.format(
-    #     os.path.basename(smiles), os.path.basename(mae_noname_file))
-
-    # subprocess.run(cmd, shell=True, cwd=os.path.dirname(smiles))
-    # if not os.path.exists(mae_noname_file):
-    #     print('ligprep failed on {}.'.format(smiles))
-    #     print(cmd)
-    #     return
     ligprocess(smiles, sdf_file)
+
+def ligsplit(big_sdf, root, name_prop='BindingDB MonomerID'):
+    if os.path.splitext(big_sdf)[-1] == ".gz":
+        big_sdf_data = gzip.open(big_sdf)
+    else:
+        big_sdf_data = open(big_sdf,'rb')
+    sts = Chem.ForwardSDMolSupplier(big_sdf_data)
+    for count, st in enumerate(sts):
+        het_id = st.GetProp('Ligand HET ID in PDB')
+        name = None
+        if len(het_id): # Need to check if a crystal ligand
+            pdbs = sorted(st.GetProp('PDB ID(s) for Ligand-Target Complex').strip().split(','))
+            for i, pdb in enumerate(pdbs):
+                if os.path.isfile(f"structures/ligands/{pdb}_lig.sdf"):
+                    name = pdb
+                    break
+        if name is None:
+            name = st.GetProp('BindingDB MonomerID')
+
+        Chem.EmbedMolecule(st)
+        assert st.GetConformer().Is3D(), f"can't make {name} into 3d"
+
+        writer = Chem.SDWriter(f"{root}/{name}.sdf")
+        writer.write(st)
+        writer.close()
+
+    print(f"Created {count+1} ligands in {root}")
 
 if __name__ == '__main__':
     import sys
