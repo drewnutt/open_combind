855194143ee84ff939b36cd7e8bf095497dac5ed
Drew McNutt <anm329@pitt.edu>
2022-05-20 11:38:05 -0400

    ProDy now for alignment
    One less external dependency. Stopped using pymol in favor of a prody
    option for alignment. Pymol was also having issues with peptide ligands,
    so this is much better.

--- open_combind/dock/struct_align.py
+++ open_combind/dock/struct_align.py
@@ -1,5 +1,5 @@
 import os
-from pymol.cmd import load, select, align, save, delete
+from prody import parsePDB, writePDB, matchChains, calcTransformation
 
 def align_successful(out_dir, struct):
     if not os.path.exists('{0}/{1}/{1}_aligned.pdb'.format(out_dir, struct)):
@@ -17,8 +17,14 @@ def struct_align(template, structs, dist=15.0, retry=True,
         print('template not processed', template_path)
         return
 
-    load(f'{template_path}', 'target')
-    select('target_to_align', f'(target and name CA) within {dist} of het')
+    template_st = parsePDB(template_path)
+    template_liginfo_path = template_path.replace(f'processed/{template}', 'raw').replace('_complex.pdb','.info')
+    temp_liginfo = open(template_liginfo_path,'r').readlines()
+    if len(temp_liginfo[0].strip('\n')) < 4:
+        selection_text = 'hetatm'
+    else:
+        selection_text = temp_liginfo[1]
+    template_to_align = template_st.select(f'calpha within {dist} of {selection_text})')
     for struct in structs:
         query_path = filtered_protein.format(pdbid=struct)
         if align_successful(align_dir, struct):
@@ -37,15 +43,19 @@ def struct_align(template, structs, dist=15.0, retry=True,
         os.system('cp {} {}/{}'.format(template_path, _workdir, _template_fname))
         os.system('cp {} {}/{}'.format(query_path, _workdir, _query_fname))
 
-        load(f'{_workdir}/{_query_fname}', 'query')
-        select('query_to_align', f'(query and name CA) within {dist} of het')
-        rms_aft, _,_, rms_bef, _, _, _ = align('query_to_align','target_to_align')
+        query = parsePDB(f'{_workdir}/{_query_fname}')
+        query_liginfo_path = query_path.replace(f'processed/{struct}','raw').replace('_complex.pdb','.info')
+        q_liginfo = open(query_liginfo_path,'r').readlines()
+        if len(q_liginfo[0].strip('\n')) < 4:
+            selection_text = 'hetatm'
+        else:
+            selection_text = q_liginfo[1]
+        query_to_align = query.select(f'calpha within {dist} of {selection_text}')
+        query_match, template_match, _ ,_ = matchChains(query_to_align,template_to_align,pwalign=True,seqid=10,overlap=10)[0]
+        transform = calcTransformation(query_match,template_match)
+        query_aligned = transform.apply(query)
 
-        if rms_bef == 0 or rms_aft < rms_bef:
-            save(aligned_prot.format(pdbid=struct),'query')
-
-        # Doesn't seem to overload objects, so just delete the query
-        delete('query')
+        writePDB(aligned_prot.format(pdbid=struct),query_aligned)
 
         if retry and not align_successful(align_dir, struct):
             print('Alignment failed. Trying again with a larger radius.')
