e6c25362c37d93733c4c0247a746268eb3306a08
Drew McNutt <anm329@pitt.edu>
2022-06-09 16:51:01 -0400

    trying to fix the CI pipeline

--- setup.py
+++ setup.py
@@ -46,7 +46,8 @@ setup(
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     url='https://github.com/drewnutt/open_combind',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    install_requires=["pandas","numpy","click","glob","plumbum",
+                        "ProDy>=2.0",],              # Required packages, pulls from pip if needed; do not use for Conda deployment
     # platforms=['Linux',
     #            'Mac OS-X',
     #            'Unix',
