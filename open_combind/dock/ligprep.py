c877003761426026c1890342aa85906ffca46131
Drew McNutt <anm329@pitt.edu>
2022-06-28 19:27:36 -0400

    safely generate conformers and user changeable number of iterations

--- open_combind/dock/ligprep.py
+++ open_combind/dock/ligprep.py
@@ -2,24 +2,33 @@ import os
 import gzip
 from rdkit.Chem import AllChem as Chem
 
-def make3DConf(inmol, confgen='etkdg_v2', ff='UFF', num_confs=10,):
+def construct_set_conformers(mol, num_confs, confgen):
+    if confgen == 'etkdg_v1':
+        ps = Chem.ETKDG()
+    elif confgen == 'etkdg_v2':
+        ps = Chem.ETKDGv2()
+    else:
+        print('confgen not etkdg v1 or 2 so using etdg')
+        ps = Chem.ETDG()
+    cids = Chem.EmbedMultipleConfs(mol, num_confs, ps )
+    if len(cids) == 0:
+        ps.useRandomCoords = True
+        cids = Chem.EmbedMultipleConfs(mol, num_confs, ps )
+    return cids
+
+def make3DConf(inmol, confgen='etkdg_v2', ff='UFF', num_confs=10, maxIters=500):
     mol = Chem.Mol(inmol)
     Chem.SanitizeMol(mol)
     mol = Chem.AddHs(mol)
     if num_confs > 0:
-        if confgen == 'etkdg_v1':
-            cids = Chem.EmbedMultipleConfs(mol, num_confs, Chem.ETKDG())
-        elif confgen == 'etkdg_v2':
-            cids = Chem.EmbedMultipleConfs(mol, num_confs, Chem.ETKDGv2())
-        else:
-            cids = Chem.EmbedMultipleConfs(mol, num_confs)
+        cids = construct_set_conformers(mol, num_confs, confgen)
     else:
         cids = [-1]
     assert len(cids) > 0 
     cenergy = []
     for conf in cids:
         if ff == 'UFF':
-            converged = not Chem.UFFOptimizeMolecule(mol, confId=conf)
+            converged = not Chem.UFFOptimizeMolecule(mol, confId=conf, maxIters=maxIters)
             cenergy.append(Chem.UFFGetMoleculeForceField(mol, confId=conf).CalcEnergy())
         elif ff == 'MMFF':
             converged = not Chem.MMFFOptimizeMolecule(mol, confId=conf)
@@ -34,21 +43,21 @@ def make3DConf(inmol, confgen='etkdg_v2', ff='UFF', num_confs=10,):
     return mol, best_conf, cenergy[best_conf]
 
 
-def write3DConf(inmol, out_fname, confgen='etkdg_v2', ff='UFF', num_confs=10,):
+def write3DConf(inmol, out_fname, confgen='etkdg_v2', ff='UFF', num_confs=10, maxIters=500):
     mol, best_conf, _ = make3DConf(inmol, num_confs=num_confs,
-                                confgen=confgen, ff=ff)
+                                confgen=confgen, ff=ff, maxIters=maxIters)
 
     writer = Chem.SDWriter(out_fname)
     writer.write(mol, best_conf)
     writer.close()
 
-def ligprocess(input_file, output_file, confgen='etkdg_v2', ff='UFF', num_confs=10):
+def ligprocess(input_file, output_file, confgen='etkdg_v2', ff='UFF', num_confs=10,maxIters=500):
     input_info = open(input_file).readlines()
     if len(input_info) == 1:
         mol = Chem.MolFromSmiles(input_info[0].strip())
         mol.SetProp('_Name', os.path.basename(input_file).replace('.smi', ''))
 
-        write3DConf(mol, output_file, confgen=confgen, ff=ff, num_confs=num_confs)
+        write3DConf(mol, output_file, confgen=confgen, ff=ff, num_confs=num_confs, maxIters=maxIters)
     else:
         writer = Chem.SDWriter(output_file)
         for line in input_info:
@@ -57,7 +66,7 @@ def ligprocess(input_file, output_file, confgen='etkdg_v2', ff='UFF', num_confs=
             mol = Chem.MolFromSmiles(smile)
             mol.SetProp('_Name', name)
 
-            mol, best_conf = make3DConf(mol, confgen=confgen, ff=ff, num_confs=num_confs)
+            mol, best_conf = make3DConf(mol, confgen=confgen, ff=ff, num_confs=num_confs, maxIters=maxIters)
        
             writer.write(mol, best_conf)
 
@@ -68,7 +77,7 @@ def ligprep(smiles,num_confs=10,confgen='etkdg_v2'):
     ligprocess(smiles, sdf_file, num_confs=num_confs, confgen=confgen)
 
 def ligsplit(big_sdf, root, multiplex=False, name_prop='BindingDB MonomerID',
-        confgen='etkdg_v2', ff='UFF', processes=1,num_confs=10):
+        confgen='etkdg_v2', ff='UFF', processes=1,num_confs=10,maxIters=500):
     if os.path.splitext(big_sdf)[-1] == ".gz":
         big_sdf_data = gzip.open(big_sdf)
     else:
@@ -88,7 +97,7 @@ def ligsplit(big_sdf, root, multiplex=False, name_prop='BindingDB MonomerID',
             name = ligand.GetProp(name_prop)
         _sdf = f"{root}/{name}.sdf"
         if not os.path.exists(_sdf):
-            unfinished.append((ligand, _sdf, confgen, ff, num_confs))
+            unfinished.append((ligand, _sdf, confgen, ff, num_confs, maxIters))
 
     if not multiplex:
         from utils import mp
@@ -100,7 +109,7 @@ def ligsplit(big_sdf, root, multiplex=False, name_prop='BindingDB MonomerID',
             print(f"Creating {output_file} with {len(unfinished)} ligands")
             writer = Chem.SDWriter(output_file)
             for lig, _, confgen, ff in unfinished:
-                mol, best_conf = make3DConf(lig, confgen=confgen, ff=ff, num_confs=num_confs)
+                mol, best_conf = make3DConf(lig, confgen=confgen, ff=ff, num_confs=num_confs, maxIters=maxIters)
            
                 writer.write(mol, best_conf)
 
