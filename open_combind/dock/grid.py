eb3ec350f40c0f33bea9f461135147b673167ee6
Drew McNutt <anm329@pitt.edu>
2022-07-31 11:57:35 -0400

    Docking more similar to original ComBind
    Now find the centroid of the co-crystallized ligand and set that to
    center of the docking box. Then give the box a size of 20x20x20. Might
    need fine tuning on size of box.

--- open_combind/dock/grid.py
+++ open_combind/dock/grid.py
@@ -1,22 +1,27 @@
 import os
-import subprocess
-from glob import glob
+from rdkit.Chem.rdMolTransforms import ComputeCentroid
+from rdkit.Chem.rdmolfiles import MolFromMolFile
 
 CMD = "gnina -r {prot} --autobox_ligand {abox_lig} "
+CMD = "gnina -r {prot} --center_x {lig_x} --center_y {lig_y} --center_z {lig_z} --size_x {box_x} --size_y {box_y} --size_z {box_z} "
 
 def make_grid(pdb,
               PROTFILE='structures/proteins/{pdb}_prot.pdb',
               LIGFILE='structures/ligands/{pdb}_lig.sdf',
-              DOCKTEMP='structures/template/{pdb}.template'):
+              DOCKTEMP='structures/template/{pdb}.template',
+              box_dimension=20):
 
     ligfile = os.path.abspath(LIGFILE.format(pdb=pdb))
     protfile = os.path.abspath(PROTFILE.format(pdb=pdb))
     docktemp = os.path.abspath(DOCKTEMP.format(pdb=pdb))
-    cmd = CMD.format(prot=protfile,abox_lig=ligfile)
+
+    center_tuple = get_mol_center(ligfile)
+    cmd = CMD.format(prot=protfile, lig_x=center_tuple[0], lig_y=center_tuple[1], lig_z=center_tuple[2],
+            box_x=box_dimension, box_x=box_dimension, box_z=box_dimension)
 
     if not (os.path.exists(ligfile) and os.path.exists(protfile)):
         print(ligfile, protfile)
-        return # Not ready.
+        return  # Not ready.
 
     print('making grid', pdb)
 
@@ -25,3 +30,7 @@ def make_grid(pdb,
 
     with open(docktemp, 'w') as template:
         template.write(cmd)
+
+def get_mol_center(ligfile):
+    mol = MolFromMolFile(ligfile)
+    return tuple(ComputeCentroid(mol.GetConformer(0)))
