d937fe008ed624dd76b8ae5da418f66313096f54
Drew McNutt <anm329@pitt.edu>
2022-07-31 17:37:50 -0400

    format string and bugfix

--- open_combind/dock/grid.py
+++ open_combind/dock/grid.py
@@ -2,8 +2,8 @@ import os
 from rdkit.Chem.rdMolTransforms import ComputeCentroid
 from rdkit.Chem.rdmolfiles import MolFromMolFile
 
-CMD = "gnina -r {prot} --autobox_ligand {abox_lig} "
-CMD = "gnina -r {prot} --center_x {lig_x} --center_y {lig_y} --center_z {lig_z} --size_x {box_x} --size_y {box_y} --size_z {box_z} "
+# CMD = "gnina -r {prot} --autobox_ligand {abox_lig} "
+CMD = "gnina -r {prot} --center_x {lig_x:.3f} --center_y {lig_y:.3f} --center_z {lig_z:.3f} --size_x {box_x} --size_y {box_y} --size_z {box_z} "
 
 def make_grid(pdb,
               PROTFILE='structures/proteins/{pdb}_prot.pdb',
@@ -17,7 +17,7 @@ def make_grid(pdb,
 
     center_tuple = get_mol_center(ligfile)
     cmd = CMD.format(prot=protfile, lig_x=center_tuple[0], lig_y=center_tuple[1], lig_z=center_tuple[2],
-            box_x=box_dimension, box_x=box_dimension, box_z=box_dimension)
+            box_x=box_dimension, box_y=box_dimension, box_z=box_dimension)
 
     if not (os.path.exists(ligfile) and os.path.exists(protfile)):
         print(ligfile, protfile)
