b27c413c473c975ca552736f6235a85c1c655571
Drew McNutt <anm329@pitt.edu>
2023-03-02 16:57:59 -0500

    Pymol scripts now work with Open-Combind
    show_interactions doesn't work fully, need to find a way to specify the
    atom involved on the ligand side. SDF files do not have a name to
    specify the atom and atom type + distance is not sufficient enough to
    specify the atom.

--- open_combind/pymol/interactions.py
+++ open_combind/pymol/interactions.py
@@ -3,95 +3,104 @@ import sys
 import pandas as pd
 
 def style():
-	cmd.show('cartoon')
-	cmd.show('lines')
-	cmd.hide('sticks')
-	cmd.util.cbaw()
-	cmd.color('slate', 'het and element C')
-	cmd.hide('everything', 'element H and (element C extend 1)')
+    cmd.show('cartoon')
+    cmd.show('lines')
+    cmd.hide('sticks')
+    cmd.util.cbaw()
+    cmd.color('slate', 'het and element C')
+    cmd.hide('everything', 'element H and (element C extend 1)')
 
-def pose_name(group, pose):
-	if pose == 0:
-		pose = group.split('-')[0]
-	elif pose < 10:
-		pose = '_0{}'.format(pose)
-	else:
-		pose = '_{}'.format(pose)
-	return '{}*.*{}'.format(group, pose)
+def pose_name(bn, pose):
+    if pose == 0:
+        pose = ''
+    elif pose < 10:
+        pose = '0{}'.format(pose)
+    else:
+        pose = '{}'.format(pose)
+    return '{}{}'.format(bn, pose)
 
 def enable(group, pose, prot=True):
-	cmd.enable(group+'*-to-*')
-	cmd.disable(group+'*-to-*.*')
-	cmd.enable(pose_name(group, pose))
-	if prot:
-		cmd.enable('{}*.*_prot'.format(group))
+    cmd.enable(group+'*-to-*')
+    cmd.disable(group+'*-to-*.*')
+    cmd.enable(pose_name(group, pose))
+    if prot:
+        cmd.enable('{}*.*_prot'.format(group))
 
-def show_interactions(ifp_file, interaction, lig, pose, delete=True, disable=True):
-	pose = int(pose)
+def show_interactions(ifp_file, interaction, protein, lig, pose, delete=True, disable=True):
+    """
+    USAGE
 
-	if delete:
-		cmd.delete('dist*')
-		cmd.delete('ps*')
-	if disable:
-		cmd.disable('*')
-	style()
+    show_interactions interaction_file, interaction, prot, ligand, pose_number,[delete=True],[disable=True]
 
-	enable(lig, pose)
+    interaction is the type of interaction to show; can be one of: all, sb, hbond, contact
+    """
+    if interaction not in ['all','hbond','contact','sb']: raise IOError('interaction must be one of: all, hbond, contact, sb')
 
-	if interaction == 'all':
-		for interaction in ['sb', 'hbond', 'contact', 'pipi']:
-			 show_interactions(ifp_file, interaction, lig, pose,
-			                   delete=False, disable=False)
-	
-	df = pd.read_csv(ifp_file)
+    pose = int(pose)
 
-	if interaction == 'hbond':
-		idx = df['label'] == 'hbond_acceptor'
-		idx |= df['label'] == 'hbond_donor'
-		thresh = 3.5
-		color = 'yellow'
-	elif interaction == 'sb':
-		idx = df['label'] == 'saltbridge'
-		thresh = 4
-		color = 'magenta'
-	elif interaction == 'contact':
-		idx = df['label'] == 'contact'
-		thresh = 1.25
-		color='smudge'
-	elif interaction == 'pipi':
-		idx = df['label'] == 'pipi'
-		idx |= df['label'] == 'pi-t'
-		thresh = 7.0
-		color='green'
+    if delete:
+        cmd.delete('dist*')
+        cmd.delete('ps*')
+    if disable:
+        cmd.disable('*')
+    style()
 
-	idx &= df['pose'] == pose
+    enable(lig, pose)
 
-	for i, row in df[idx].iterrows():
-		if interaction == 'contact' and row['dist'] > thresh*row['vdw']: continue
-		if interaction != 'contact' and row['dist'] > thresh: continue
-		
-		chain, resid, _, _ = row['protein_res'].split(':')
-		prot = '{}*.*prot and chain {} and resid {} and name {}'.format(lig,
-		                                                               chain,
-		                                                               resid,
-		                                                               row['protein_atom'].replace(',', '+'))
-		ligand = '{} and name {}'.format(pose_name(lig, pose), row['ligand_atom'].replace(',', '+'))
+    if interaction == 'all':
+        for interaction in ['sb', 'hbond', 'contact']: #, 'pipi']:
+             show_interactions(ifp_file, interaction, lig, pose,
+                               delete=False, disable=False)
+    
+    df = pd.read_csv(ifp_file)
 
-		print(prot, ligand)
+    if interaction == 'hbond':
+        idx = df['label'] == 'hbond_acceptor'
+        idx |= df['label'] == 'hbond_donor'
+        thresh = 3.5
+        color = 'yellow'
+    elif interaction == 'sb':
+        idx = df['label'] == 'saltbridge'
+        thresh = 4
+        color = 'magenta'
+    elif interaction == 'contact':
+        idx = df['label'] == 'contact'
+        thresh = 1.25
+        color='smudge'
+    elif interaction == 'pipi':
+        idx = df['label'] == 'pipi'
+        idx |= df['label'] == 'pi-t'
+        thresh = 7.0
+        color='green'
 
-		cmd.pseudoatom('ps{}{}prot'.format(interaction, i), prot)
-		cmd.pseudoatom('ps{}{}lig'.format(interaction, i), ligand)
+    idx &= df['pose'] == pose
 
-		cmd.dist('dist'+interaction+str(i),
-		         'ps{}{}prot'.format(interaction, i),
-		         'ps{}{}lig'.format(interaction, i))
-		cmd.color(color, 'dist'+interaction+str(i))
+    for i, row in df[idx].iterrows():
+        if interaction == 'contact' and row['dist'] > thresh*row['vdw']: continue
+        if interaction != 'contact' and row['dist'] > thresh: continue
+        
+        chain, resid, _, _ = row['protein_res'].split(':')
+        prot = '{} and chain {} and resid {} and name {}'.format(protein,
+                                                                       chain,
+                                                                       resid,
+                                                                       row['protein_atom'].replace(',', '+'))
+        print(row['dist'])
+        ligand = '{} and {}'.format(pose_name(lig, pose), str(row['ligand_atom']).replace(',', '+'))
 
-	cmd.set('dash_width', 6)
-	cmd.set('dash_width', 3, 'distcontact*')
+        print(prot, ligand)
 
-	cmd.enable('{}*.*prot'.format(lig))
-	cmd.enable(pose_name(lig, pose))
-	cmd.enable(lig)
+        cmd.pseudoatom('ps{}{}prot'.format(interaction, i), prot)
+        cmd.pseudoatom('ps{}{}lig'.format(interaction, i), ligand)
+
+        cmd.dist('dist'+interaction+str(i),
+                 'ps{}{}prot'.format(interaction, i),
+                 'ps{}{}lig'.format(interaction, i))
+        cmd.color(color, 'dist'+interaction+str(i))
+
+    cmd.set('dash_width', 6)
+    cmd.set('dash_width', 3, 'distcontact*')
+
+    cmd.enable(protein)
+    cmd.enable(pose_name(lig, pose))
 
 cmd.extend('show_interactions', show_interactions)
