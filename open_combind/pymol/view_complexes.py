36581d25ba06197f0a516ce53dab2a2e0cbdb178
Drew McNutt <anm329@pitt.edu>
2023-03-02 16:57:59 -0500

    Pymol scripts now work with Open-Combind
    show_interactions doesn't work fully, need to find a way to specify the
    atom involved on the ligand side. SDF files do not have a name to
    specify the atom and atom type + distance is not sufficient enough to
    specify the atom.

--- open_combind/pymol/view_complexes.py
+++ open_combind/pymol/view_complexes.py
@@ -2,8 +2,15 @@ from pymol import cmd
 from glob import glob
 
 def load_complexes(protein, n=1):
+    """
+    USAGE
+
+    load_complexes protein,[n=1]
+
+    loads the first n protein ligand complexes from the directory [protein].
+    """
     n = int(n)
-    for prot in sorted(glob('{}/structures/proteins/*_prot.mae'.format(protein)))[:n]:
+    for prot in sorted(glob('{}/structures/proteins/*_prot.pdb'.format(protein)))[:n]:
         pdb = prot.split('/')[-1].split('_')[0]
         load_crystal_protein(protein, pdb)
         load_crystal_pose(protein, pdb)
@@ -21,44 +28,55 @@ def load_complexes(protein, n=1):
     cmd.hide('everything', 'name H')
 
 def load_crystal_protein(protein, ligand):
-    cmd.load('{}/structures/proteins/{}_prot.mae'.format(protein, ligand))
+    cmd.load('{}/structures/proteins/{}_prot.pdb'.format(protein, ligand))
     cmd.set_name('{}_prot'.format(ligand), 'prot_{}'.format(ligand))
 
 def load_crystal_pose(protein, ligand):
-    cmd.load('{}/structures/ligands/{}_lig.mae'.format(protein, ligand, ligand))
+    cmd.load('{}/structures/ligands/{}_lig.sdf'.format(protein, ligand, ligand))
     cmd.set_name('{}_lig'.format(ligand), 'crystal_{}'.format(ligand))
 
 ###################################################################
-def load_pose(protein, ligand, struct, pose, prefix):
-    pv = glob('{}/docking/{}/{}*{}/*pv.maegz'.format(protein, 'confgen_es4', ligand, struct))[0]
-    name = pv.split('/')[-1].split('.')[0]
+def load_pose(protein, ligand, pose, prefix):
+    pv = glob('{}/dock_out/{}.sdf.gz'.format(protein, ligand))[0]
+    lig_name = ligand.split('-to-')[0]
     
-    cmd.load(pv)
-    cmd.ungroup('*')
+    cmd.load(pv,ligand)
+    cmd.split_states(ligand)
+    cmd.delete(ligand)
     if pose == 0:
-        obj = '{}.{}_lig'.format(name, ligand)
+        pose = ''
     elif pose < 10:
-        obj = '{}.{}_lig_0{}'.format(name, ligand, pose)
+        pose = '0' + str(pose)
     else:
-        obj = '{}.{}_lig_{}'.format(name, ligand, pose)
-    cmd.set_name(obj, '{}_{}'.format(prefix, ligand))
-    cmd.delete(name + '*')
+        pose = str(pose)
+    print(ligand, pose, prefix, ligand)
+    cmd.set_name(lig_name + pose, '{}_{}'.format(prefix, ligand))
+    cmd.delete(lig_name + '*')
 
-def load_top_glide(protein, n = 1):
+def load_top_docked(protein, n = 1):
     n = int(n)
     grid = None
-    for prot in sorted(glob('{}/structures/proteins/*_prot.mae'.format(protein)))[:n]:
+    for prot in sorted(glob('{}/structures/proteins/*_prot.pdb'.format(protein)))[:n]:
         pdb = prot.split('/')[-1].split('_')[0]
         if grid is None:
             grid = pdb
         print(pdb)
-        load_pose(protein, pdb, grid, 0, 'glide')
-    cmd.show('sticks', "glide_*")
+        load_pose(protein, pdb, grid, 0, 'docked')
+    cmd.show('sticks', "docked_*")
     cmd.hide('lines', 'element h')
     cmd.hide('everything', 'element H and not (element N+O extend 1)')
 
 def load_results(protein, scores):
-    struct = glob('{}/docking/grids/*'.format(protein))[0].split('/')[-1]
+    """
+    USAGE
+
+    load_results protein, pose_prediction_csv
+
+    load all results from the ComBind docking for the ligands in [pose_prediction_csv]. Additionally, loads crystal structures for non-CHEMBL ligands
+
+    Single ligand docking results are in yellow, ComBind results are in cyan, and crystal structures are in white
+    """
+    struct = glob('{}/structures/template/*'.format(protein))[0].split('/')[-1].split('.')[0]
     load_crystal_protein(protein, struct)
     with open(scores) as fp:
         fp.readline()
@@ -66,22 +84,23 @@ def load_results(protein, scores):
             if line[:3] == 'com': continue
             (ligand,
              combind_rank, combind_rmsd,
-             glide_rank, glide_rmsd,
-             best_rank, best_rmsd) = line.strip().split(',')
-            ligand = ligand.replace('_lig', '')
-            load_pose(protein, ligand, struct, int(combind_rank), 'combind')
-            load_pose(protein, ligand, struct, int(glide_rank), 'glide')
+             docked_rmsd,
+             best_rmsd) = line.strip().split(',')
+            load_pose(protein, ligand, int(combind_rank), 'combind')
+            load_pose(protein, ligand, 0, 'docked')
+            ligand = ligand.split('-to-')[0].replace('_lig','')
             if ligand[:6] != 'CHEMBL':
                 load_crystal_pose(protein, ligand)
+            cmd.group(ligand,f'*{ligand}*')
 
-    cmd.show('sticks', "glide_*")
+    cmd.show('sticks', "docked_*")
     cmd.show('sticks', "combind_*")
     cmd.show('sticks', "crystal_*")
     cmd.hide('lines', 'element h')
     cmd.hide('everything', 'element H and not (element N+O extend 1)')
 
     cmd.util.cbaw('*')
-    cmd.color('yellow', 'glide* and element c')
+    cmd.color('yellow', 'docked* and element c')
     cmd.color('cyan', 'combind* and element c')
     cmd.set('stick_radius', '0.13')
     
@@ -92,6 +111,7 @@ def load_results(protein, scores):
 
 ###############################################################
 
+## not sure what this stuff is for?
 def parse_fp_file(fp_file):
     ifps = {}
     try:
@@ -131,6 +151,6 @@ def show_interactions(protein, ligand, struct, ifp, pose):
         cmd.label('{}/ca'.format(res), score)
     
 cmd.extend('load_complexes', load_complexes)
-cmd.extend('load_top_glide', load_top_glide)
+cmd.extend('load_top_docked', load_top_docked)
 cmd.extend('load_results', load_results)
-cmd.extend('show_interactions', show_interactions)
+# cmd.extend('show_interactions', show_interactions)
