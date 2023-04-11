9f2a9e77eb57262ddbff26cbaed48e41775a486a
drewnutt <anm329@pitt.edu>
2023-04-11 14:35:11 -0400

    no custom atom types for docking

--- open_combind/dock/grid.py
+++ open_combind/dock/grid.py
@@ -3,8 +3,7 @@ import pkg_resources
 from rdkit.Chem.rdMolTransforms import ComputeCentroid
 from rdkit.Chem.rdmolfiles import MolFromMolFile
 
-CMD = "gnina -r {prot} --autobox_ligand {abox_lig} --custom_atoms {atom_file} "
-# CMD = "gnina -r {prot} --center_x {lig_x:.3f} --center_y {lig_y:.3f} --center_z {lig_z:.3f} --size_x {box_x} --size_y {box_y} --size_z {box_z} "
+CMD = "gnina -r {prot} --autobox_ligand {abox_lig} " 
 
 def make_grid(pdb,
               PROTFILE='structures/proteins/{pdb}_prot.pdb',
@@ -16,16 +15,13 @@ def make_grid(pdb,
     ligfile = os.path.abspath(LIGFILE.format(pdb=pdb))
     protfile = os.path.abspath(PROTFILE.format(pdb=pdb))
     docktemp = os.path.abspath(DOCKTEMP.format(pdb=pdb))
-    if CUSTOMATOMS is None:
+    if CUSTOMATOMS is None and '{atom_file}' in CMD:
         atom_file = pkg_resources.resource_filename(__name__, "crossdock_atom_types.txt")
     else:
         atom_file = os.path.abspath(CUSTOMATOMS)
 
 
-    # center_tuple = get_mol_center(ligfile)
     cmd = CMD.format(prot=protfile, abox_lig=ligfile, aadd=box_add, atom_file=atom_file)
-            # lig_x=center_tuple[0], lig_y=center_tuple[1], lig_z=center_tuple[2],
-            # box_x=box_dimension, box_y=box_dimension, box_z=box_dimension)
 
     if not (os.path.exists(ligfile) and os.path.exists(protfile)):
         print(ligfile, protfile)
