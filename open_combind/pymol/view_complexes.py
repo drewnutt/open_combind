e1e3c9845ed32b1cb07b6c52716697d25363eabb
Drew McNutt <anm329@pitt.edu>
2023-04-14 15:31:16 -0400

    one script for all the plugins

--- open_combind/pymol/view_complexes.py
+++ open_combind/pymol/view_complexes.py
@@ -1,4 +1,8 @@
 from pymol import cmd
+from rdkit import Chem
+import gzip
+import sys
+import pandas as pd
 from glob import glob
 
 def load_complexes(protein, n=1):
@@ -30,6 +34,7 @@ def load_complexes(protein, n=1):
 def load_crystal_protein(protein, ligand):
     cmd.load('{}/structures/proteins/{}_prot.pdb'.format(protein, ligand))
     cmd.set_name('{}_prot'.format(ligand), 'prot_{}'.format(ligand))
+    return 'prot_' + ligand 
 
 def load_crystal_pose(protein, ligand):
     cmd.load('{}/structures/ligands/{}_lig.sdf'.format(protein, ligand, ligand))
@@ -37,7 +42,8 @@ def load_crystal_pose(protein, ligand):
 
 ###################################################################
 def load_pose(protein, ligand, pose, prefix):
-    pv = glob('{}/dock_out/{}.sdf.gz'.format(protein, ligand))[0]
+    ligand_file ='{}/dock_out/{}-docked.sdf.gz'.format(protein, ligand)
+    pv = glob(ligand_file)[0]
     lig_name = ligand.split('-to-')[0]
     
     cmd.load(pv,ligand)
@@ -52,6 +58,7 @@ def load_pose(protein, ligand, pose, prefix):
     print(ligand, pose, prefix, ligand)
     cmd.set_name(lig_name + pose, '{}_{}'.format(prefix, ligand))
     cmd.delete(lig_name + '*')
+    return ligand_file
 
 def load_top_docked(protein, n = 1):
     n = int(n)
@@ -66,7 +73,7 @@ def load_top_docked(protein, n = 1):
     cmd.hide('lines', 'element h')
     cmd.hide('everything', 'element H and not (element N+O extend 1)')
 
-def load_results(protein, scores):
+def load_results(protein, scores, show_inter:bool=False, show_chembl_inter:bool=False):
     """
     USAGE
 
@@ -77,7 +84,7 @@ def load_results(protein, scores):
     Single ligand docking results are in yellow, ComBind results are in cyan, and crystal structures are in white
     """
     struct = glob('{}/structures/template/*'.format(protein))[0].split('/')[-1].split('.')[0]
-    load_crystal_protein(protein, struct)
+    prot_obj = load_crystal_protein(protein, struct)
     with open(scores) as fp:
         fp.readline()
         for line in fp:
@@ -86,8 +93,13 @@ def load_results(protein, scores):
              combind_rank, combind_rmsd,
              docked_rmsd,
              best_rmsd) = line.strip().split(',')
-            load_pose(protein, ligand, int(combind_rank), 'combind')
+            ligand_file = load_pose(protein, ligand, int(combind_rank), 'combind')
             load_pose(protein, ligand, 0, 'docked')
+            if show_inter and ('CHEMBL' not in ligand_file or show_chembl_inter):
+                print(ligand_file)
+                interaction_file = ligand_file.split('.sdf.gz')[0]+'_ifp_rd1_raw.csv'
+                show_interactions(interaction_file, 'all', prot_obj, ligand, ligand_file, int(combind_rank))
+                show_interactions(interaction_file, 'all', prot_obj, ligand, ligand_file, 0)
             ligand = ligand.split('-to-')[0].replace('_lig','')
             if ligand[:6] != 'CHEMBL':
                 load_crystal_pose(protein, ligand)
@@ -110,47 +122,115 @@ def load_results(protein, scores):
     cmd.set('cartoon_transparency', '0.5')
 
 ###############################################################
+def style():
+    cmd.show('cartoon')
+    cmd.show('lines')
+    cmd.hide('sticks')
+    cmd.util.cbaw()
+    cmd.color('slate', 'het and element C')
+    cmd.hide('everything', 'element H and (element C extend 1)')
 
-## not sure what this stuff is for?
-def parse_fp_file(fp_file):
-    ifps = {}
-    try:
-        with open(fp_file) as f:
-            pose_num = 0
-            for line in f:
-                if line.strip() == '': continue
-                if line[:4] == 'Pose':
-                    pose_num = int(line.strip().split(' ')[1])
-                    ifps[pose_num] = {}
-                    continue
-                sc_key, sc = line.strip().split('=')
-                i,r,ss = sc_key.split('-')
-                i = int(i)
-                sc = float(sc)
-                prev_sc = ifps[(i, r)] if (i,r) in ifps[pose_num] else 0
-                ifps[pose_num][(i,r)] = max(prev_sc, sc)
+def pose_name(bn, pose):
+    if pose == 0:
+        pose = ''
+    elif pose < 10:
+        pose = '0{}'.format(pose)
+    else:
+        pose = '{}'.format(pose)
+    return '{}{}'.format(bn, pose)
 
-    except Exception as e:
-        print(e)
-        print(fp_file, 'fp not found')
-    if len(ifps) == 0:
-        print('check', fp_file)
-        return {}
-    return ifps
+def enable(group, pose, prot=True):
+    cmd.enable(group+'*-to-*')
+    cmd.disable(group+'*-to-*.*')
+    cmd.enable(pose_name(group, pose))
+    if prot:
+        cmd.enable('{}*.*_prot'.format(group))
 
-def show_interactions(protein, ligand, struct, ifp, pose):
-    ifp_file = '{}/ifp/{}/{}_lig-to-{}-confgen_es4.fp'.format(protein, ifp, ligand, struct)
-    print(ifp_file)
-    ifp = parse_fp_file(ifp_file)[int(pose)]
-    cmd.hide('labels')
-    cmd.set('label_size', 50)
-    for (i, r), score in ifp.items():
-        if i not in [2, 3]: continue
-        if score < 0.5: continue
-        res = r.split(':')[1].split('(')[0]
-        cmd.label('{}/ca'.format(res), score)
+def show_interactions(ifp_file, interaction, protein, lig, ligand_file, pose, delete=True, disable=True, lig_mol = None):
+    """
+    USAGE
+
+    show_interactions interaction_file, interaction, prot, ligand, pose_number,[delete=True],[disable=True]
+
+    interaction is the type of interaction to show; can be one of: all, sb, hbond, contact
+    """
+    if interaction not in ['all','hbond','contact','sb']: raise IOError('interaction must be one of: all, hbond, contact, sb')
+
+    pose = int(pose)
+
+    if delete:
+        cmd.delete('dist*')
+        cmd.delete('ps*')
+    if disable:
+        cmd.disable('*')
+    style()
+
+    enable(lig, pose)
+
+    if lig_mol is None:
+        if isinstance(ligand_file,str) and ligand_file.endswith('.gz'):
+            ligand_file = gzip.open(ligand_file)
+        lig_mols = [mol for mol in Chem.ForwardSDMolSupplier(ligand_file)]
+        assert len(lig_mols), "No ligands found in {}".format(ligand_file)
+        lig_mol = lig_mols[pose]
+
+    if interaction == 'all':
+        for interaction in ['sb', 'hbond', 'contact']: #, 'pipi']:
+             show_interactions(ifp_file, interaction, protein, lig, ligand_file, pose,
+                               delete=False, disable=False, lig_mol= lig_mol)
+
+    
+    df = pd.read_csv(ifp_file)
+
+    if interaction == 'hbond':
+        idx = df['label'] == 'hbond_acceptor'
+        idx |= df['label'] == 'hbond_donor'
+        thresh = 3.5
+        color = 'yellow'
+    elif interaction == 'sb':
+        idx = df['label'] == 'saltbridge'
+        thresh = 4
+        color = 'magenta'
+    elif interaction == 'contact':
+        idx = df['label'] == 'contact'
+        thresh = 1.25
+        color='green'
+    elif interaction == 'pipi':
+        idx = df['label'] == 'pipi'
+        idx |= df['label'] == 'pi-t'
+        thresh = 7.0
+        color='smudge'
+
+    idx &= df['pose'] == pose
+
+    for i, row in df[idx].iterrows():
+        if interaction == 'contact' and row['dist'] > thresh*row['vdw']: continue
+        if interaction != 'contact' and row['dist'] > thresh: continue
+        
+        chain, resid, _, _ = row['protein_res'].split(':')
+        prot = '{} and chain {} and resid {} and name {}'.format(protein,
+                                                                       chain,
+                                                                       resid,
+                                                                       row['protein_atom'].replace(',', '+'))
+        ligand = [float(f) for f in lig_mol.GetConformer(-1).GetAtomPosition(row['ligand_atom'])]
+
+
+        cmd.pseudoatom('ps{}{}prot'.format(interaction, i), prot)
+        cmd.pseudoatom('ps{}{}lig'.format(interaction, i), pos=ligand)
+
+        cmd.dist('dist'+interaction+str(i),
+                 'ps{}{}prot'.format(interaction, i),
+                 'ps{}{}lig'.format(interaction, i))
+        cmd.color(color, 'dist'+interaction+str(i))
+
+    cmd.set('dash_width', 6)
+    cmd.set('dash_width', 3, 'distcontact*')
+
+    cmd.enable(protein)
+    cmd.enable(pose_name(lig, pose))
+
     
 cmd.extend('load_complexes', load_complexes)
 cmd.extend('load_top_docked', load_top_docked)
 cmd.extend('load_results', load_results)
-# cmd.extend('show_interactions', show_interactions)
+cmd.extend('show_interactions', show_interactions)
