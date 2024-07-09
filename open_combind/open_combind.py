45d9253e7c429c6dc3af58e0412c34ceee31503e
Drew McNutt <anm329@pitt.edu>
2024-07-09 12:50:31 -0400

    handling structure names

--- open_combind/open_combind.py
+++ open_combind/open_combind.py
@@ -95,8 +95,10 @@ def structprep(templ_struct='', struct='', raw_dir='structures/raw',
         raw_dir = protein_dir
     structs = glob(f'{raw_dir}/*.pdb*') + glob(f'{raw_dir}/*.cif.*')
     structs = sorted(structs, key=lambda x: x.split('/')[-1].split('.')[0])
+    if template_only:
+        structs = [struct.replace('_prot','') for struct in structs]
     assert len(structs) > 0, f'No structures found in {raw_dir}'
-    structs = [struct.split('/')[-1].split('.pdb')[0] for struct in structs]
+    # structs = [struct.split('/')[-1].split('.pdb')[0] for struct in structs]
     
     if not struct:
         struct = structs[0]
